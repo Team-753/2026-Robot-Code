625edeacd98aa63186656970b8167fd6d6377ccb
CeeTortle <christurel741@gmail.com>
2026-02-08 12:44:44 -0800

    Targeting is command now c:

--- Drivetrain/Targeting2.py
+++ Drivetrain/Targeting2.py
@@ -1,15 +1,17 @@
-import commands2,wpimath.controller
+import commands2,wpimath.controller,wpimath.trajectory
 from math import atan2,pi
 from Drivetrain.swerveSubsys import overideRobotInput,driveTrainSubsys
 class targetPointCommand(commands2.Command):
     def __init__(self,driveSubsys:driveTrainSubsys,tx,ty):
         self.tx,self.ty=tx,ty
         self.driveSubsys=driveSubsys
-        self.thetaPid=wpimath.controller.ProfiledPIDControllerRadians(3,0,0,wpimath.trajectory.TrapezoidProfileRadians.Constraints(2*pi,2*pi))
+        self.thetaPid=wpimath.controller.ProfiledPIDControllerRadians(6,0.01,0,wpimath.trajectory.TrapezoidProfileRadians.Constraints(2*pi,2*pi))
+        self.thetaPid.setIntegratorRange(0,1)
     def execute(self):
         robotPose=self.driveSubsys.getPoseState()
-        desiredRotation=atan2(robotPose.y-self.ty,robotPose.x-self.tx)
+        desiredRotation=atan2(self.ty-robotPose.y,self.tx-robotPose.x)
         output=self.thetaPid.calculate(robotPose.rotation().radians()-desiredRotation)
+        print(robotPose.rotation().radians(),desiredRotation,self.ty-robotPose.y)
         self.driveSubsys.overideInput(rot=output)
     def end(self,interrupted):
         self.driveSubsys.overideInput()
