d1b6dc30c2b0c97a8d52b7da8be16469e91c9c94
CeeTortle <christurel741@gmail.com>
2026-02-12 19:19:11 -0800

    eli - intakeSubsys additions

--- AuxilarySystems/IntakeSubsys.py
+++ AuxilarySystems/IntakeSubsys.py
@@ -40,16 +40,24 @@ class intakeSubsys(commands2.Subsystem):
         self.timer.start()
         self.targetVelocity = 0
         self.spinToggle = False
+        self.inRange = False
 
         self.AbsEncoder = wpilib.DutyCycleEncoder(0)
         self.AbsEncoder.get()
+        self.updown.set_position(self.convertRotations(self.AbsEncoder.get()))
+
+    def convertMotorRotations(self, absValue):
+        return absValue * 50 + auxiliaryConfig.intakeUpDownEncoderOffset # this math is to be determined later
         
-        
+    def convertAbsRotations(self, absValue):
+        return (absValue + auxiliaryConfig.intakeUpDownEncoderOffset) * 360
 
     def teleopInit(self):
         self.state = 'teleop'
 
     def periodic(self):
+
+        print (self.AbsEncoder.get())
 
         if self.state == 'teleop':
             self.executeState()
@@ -66,23 +74,40 @@ class intakeSubsys(commands2.Subsystem):
         self.APressed = self.controller.getAButton()
         self.AChanged = self.prevVal2 == False and self.APressed == True
 
+        self.prevVal3 = self.inRange
+        AbsEncoderConverted = self.convertAbsRotations(self.AbsEncoder.get())
+        self.inRange = (AbsEncoderConverted < auxiliaryConfig.intakeDownPosition + 15) and (AbsEncoderConverted > auxiliaryConfig.intakeDownPosition - 5)
+        self.enteredRange = self.prevVal3 == False and self.inRange == True
+
+        if not self.inRange:
+            self.spin.set_control(self.request.with_velocity(0))
+            self.spinToggle = False
+
         if self.state == 'teleop':
 
-            if self.YChanged:
+            if self.YChanged and self.inRange:
                 
                 self.spinToggle = not self.spinToggle
+                if self.spinToggle == True:
+                    self.spin.set_control(self.request.with_velocity(auxiliaryConfig.intakeSpinnerSpeed))
+                else:
+                    self.spin.set_control(self.request.with_velocity(0))
 
             if self.AChanged:
                 if self.whatDIR == False:
                     targetDirection = ((auxiliaryConfig.intakeUpPosition / 360) * 50)
+                    
                     self.whatDIR = True
                 else:
                     targetDirection = ((auxiliaryConfig.intakeDownPosition / 360) * 50)
                     self.whatDIR = False
                 # call for position move
                 self.updownController.setSetpoint(targetDirection, rev.SparkMax.ControlType.kPosition, rev.ClosedLoopSlot(0))
+                print (f'moving to {targetDirection / 50}')
+            
+            
 
             if self.timer.get() > .99 :
-                print(f'current intake velocity:{self.spin.get_velocity().value}, target velocity {self.targetVelocity}')
+                print (self.AbsEncoder.get())
                 self.timer.reset()
                 self.timer.start()            
--- AuxilarySystems/auxiliaryConfig.py
+++ AuxilarySystems/auxiliaryConfig.py
@@ -12,8 +12,9 @@ indexerSpeed=15 #revolutions per second
 indexerMotorIndexNumberLIKETHEONLYMOTOR = 99
 
 #Intake Subsystem config
-intakeSpeed=15
 intakeUpDownMotorID=98
 intakeSpinMotorID=97
 intakeDownPosition = 0  #these are in degrees
 intakeUpPosition = -120 #these are in degrees
+intakeUpDownEncoderOffset = 0 #how far off is the motor that moves the intake up and down(in rotations)
+intakeSpinnerSpeed = 30 #in rotations per second
