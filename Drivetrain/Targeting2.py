737e06398692ea6e8f399b2173a944d2ee05425e
CeeTortle <christurel741@gmail.com>
2026-02-21 11:27:40 -0800

    Configed driver button bindings, added old targeti

--- Drivetrain/Targeting2.py
+++ Drivetrain/Targeting2.py
@@ -9,6 +9,20 @@ import wpilib
 
 #Unfortunantly, Cheeseburgers-per-hour was not easy to implement into the code (plus I am lazy) -Ryan T
 class targetPointCommand(commands2.Command):
+    def __init__(self,driveSubsys:driveTrainSubsys,tx,ty):
+        self.tx,self.ty=tx,ty
+        self.driveSubsys=driveSubsys
+        self.thetaPid=wpimath.controller.ProfiledPIDControllerRadians(65,0.06,0.1,wpimath.trajectory.TrapezoidProfileRadians.Constraints(2*pi,2*pi))
+        self.thetaPid.setIntegratorRange(-1,1)
+        self.thetaPid.enableContinuousInput(-pi,pi)
+    def execute(self):
+        robotPose=self.driveSubsys.getPoseState()
+        desiredRotation=atan2(self.ty-robotPose.y,self.tx-robotPose.x)
+        output=self.thetaPid.calculate(robotPose.rotation().radians(),desiredRotation)
+        self.driveSubsys.overideInput(rot=output)
+    def end(self,interrupted):
+        self.driveSubsys.overideInput()
+class targetPointWithLeadCommand(commands2.Command):
     def __init__(self,driveSubsys:driveTrainSubsys):
         super().__init__()
         self.driveSubsys = driveSubsys
