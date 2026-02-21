55036aa9702f32a46ca0d846f9e65f9c0718afe7
CeeTortle <christurel741@gmail.com>
2026-02-21 11:44:19 -0800

    eli - updated aux configs for shooter and IntakeSubsys

--- AuxilarySystems/auxiliaryConfig.py
+++ AuxilarySystems/auxiliaryConfig.py
@@ -2,14 +2,16 @@ auxController="XboxController" #Options:XboxController
 auxControllerSlot=1
 
 #Shooter Subystem Config
-shooterIndexMotorID=6 #Rev SparkMAX
-shooterMotorID1=13 #All are Phoenix6 TalonFX
-shooterMotorID2=14
-shooterMotorID3=15
-shooterMotorID4=16
-shooterEnableBtnIdx=3 # joystick raw button index for enable button
-shooterVelocityUpBtnIdx=6 # joystick raw button index for velocity up increment
-shooterVelocityDownBtnIdx=4 # joystick raw button index for velocity down increment
+shooterIndexMotorID=18 #Rev SparkMAX
+#All are Phoenix6 TalonFX
+shooterMotorID1=14 # + velocity
+shooterMotorID2=15 # + velocity
+shooterMotorID3=13 # - velocity
+shooterMotorID4=16 # - velocity
+shooterEnableBtnIdx=3 # AXIS index for enable button
+shooterVelocityUpBtnIdx=4 # raw button index for velocity up increment
+shooterVelocityDownBtnIdx=1 # raw button index for velocity down increment
+shooterIndexDutyCycle = 0.1 # fixed duty cycle of the rev indexer
 
 #Indexer Subsystem config
 indexerSpeed=5 #revolutions per second
@@ -17,8 +19,8 @@ indexerMotorIndexNumberLIKETHEONLYMOTOR = 17
 indexerEnableBtnIdx=2
 
 #Intake Subsystem config
-intakeUpDownMotorID=9
-intakeSpinMotorID=20
+intakeUpDownMotorID=98
+intakeSpinMotorID=5
 intakeDownPosition = 0  #these are in degrees
 intakeUpPosition = 120 #these are in degrees
 intakeUpDownEncoderOffset = 0 #how far off is the motor that moves the intake up and down(in rotations)
