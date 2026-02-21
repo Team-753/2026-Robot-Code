3955b3b3aefd8bf344bf70d0f385eb69259198ba
CeeTortle <christurel741@gmail.com>
2026-02-20 20:09:42 -0800

    eli - shootersubsys updates

--- AuxilarySystems/shooterSubsys.py
+++ AuxilarySystems/shooterSubsys.py
@@ -42,8 +42,8 @@ class shooterSubsys(commands2.Subsystem):
         self.LBPressed = False
         self.timer.reset()
         self.timer.start()
-        self.targetVelocity = 2 # initial target velocity for all BigBoy's
-        self.VelocityIncrement = 1 # velocity increment when performing shooting test
+        self.targetVelocity = 1 # initial target velocity for all BigBoy's
+        self.VelocityIncrement = 0.5 # velocity increment when performing shooting test
 
     def teleopInit(self):
         self.state = 'teleop'
@@ -72,7 +72,7 @@ class shooterSubsys(commands2.Subsystem):
         
         self.prevVal = self.XPressed
         self.XPressed = self.controller.getRawAxis(3)
-        self.XChanged = self.prevVal < 0.5 and self.XPressed > 0.5
+        self.XChanged = (self.prevVal < 0.5 and self.XPressed > 0.5) or (self.prevVal > 0.5 and self.XPressed < 0.5)
 
         self.prevVal2 = self.LBPressed
         self.LBPressed = self.controller.getRawButton(auxiliaryConfig.shooterVelocityUpBtnIdx)
@@ -94,8 +94,8 @@ class shooterSubsys(commands2.Subsystem):
                 self.toggleshoot = True
                 self.bigBoy1.set_control(self.request.with_velocity(self.targetVelocity).with_feed_forward(0.2))
                 self.bigBoy2.set_control(self.request.with_velocity(self.targetVelocity).with_feed_forward(0.2))
-                self.bigBoy3.set_control(self.request.with_velocity(-self.targetVelocity).with_feed_forward(-0.2))
-                self.bigBoy4.set_control(self.request.with_velocity(-self.targetVelocity).with_feed_forward(-0.2))
+                self.bigBoy3.set_control(self.request.with_velocity(self.targetVelocity).with_feed_forward(0.2))
+                self.bigBoy4.set_control(self.request.with_velocity(self.targetVelocity).with_feed_forward(0.2))
                 self.littleone.set(auxiliaryConfig.shooterIndexDutyCycle)
                 print ('starting all shooter motors')
             elif self.XChanged and self.toggleshoot:
