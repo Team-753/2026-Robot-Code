62e8df03a8a40e1d545930f072a6880b94511155
CeeTortle <christurel741@gmail.com>
2026-03-01 11:10:54 -0800

    Kris traj added, moire auto stuffs

--- AutonomousCommands/autoCommands.py
+++ AutonomousCommands/autoCommands.py
@@ -2,4 +2,6 @@ import commands2,wpilib
 from AuxilarySystems import flipSubsys,IntakeSubsys,shooterSubsys
 def setShooting(shooterState):
     if shooterState:
-        print("settingShooter"+shooterState)
+        print("settingShooter",shooterState)
+    elif not shooterState:
+        print("stoppingShooter")
